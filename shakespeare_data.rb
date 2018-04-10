13b4fe76f23e6c8f22758c59422a7c1fc24ea1fc
MOHAMMAD-ALI <bacha@tuta.io>
2018-04-10 23:28:39 +0200

    update

--- shakespeare_data.rb
+++ shakespeare_data.rb
@@ -1,8 +1,21 @@
-
-def jean_michel_data
-  dictionnary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "low", "own", "part", "partner", "sit", "sit"]
-    dictionnary.uniq.each do |elem|
-  puts "#{elem}\t#{dictionnary.count(elem)}"
-    end
+def get_file_as_string(filename)
+  data = ''
+  f = File.open(filename, "r")
+  f.each_line do |line|
+    data += line
+  end
+  return data
 end
-jean_michel_data
+#Le texte entre dans un string
+skspr_txt = get_file_as_string 'screw_words.txt'
+#ensuite on performe dessus la recherche pour chaque mot combien de fois il apparait
+frequence = Hash.new(0)
+words = skspr_txt.tr('.,;"!\'?"', '').downcase.split(' ')
+words.each do |word|
+  frequence[word] += 1
+  end
+
+
+  a = frequence.sort_by { |word, count| count }
+  a = a.reverse
+  puts a.inspect
