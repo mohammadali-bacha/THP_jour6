bdea958bdb3fa703a3f0fcdc041dd7e77d8af026
MOHAMMAD-ALI <bacha@tuta.io>
2018-04-10 15:49:07 +0200

    update perform method

---
+++ Stock-picker.rb
@@ -0,0 +1,26 @@
+def trader_du_dimanche
+  array =  [17,3,6,9,15,8,6,1,10]
+  puts array.max
+
+end
+
+
+
+
+
+
+
+
+
+
+def trader_du_lundi
+
+end
+
+
+def perform
+  trader_du_dimanche
+  trader_du_lundi
+end
+
+perform
